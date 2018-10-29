8c7e3e74b3982e22947aefab2ec74c9c0db4790a
Shayane Umar <shayane.umar@gmail.com>
2018-10-29 12:42:40 +0530

    Simulate problem in app

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -6,20 +6,26 @@ import { Aux3Component } from './aux3/aux3.component';
 
 const routes: Routes = [
   {
-    path: 'aux1',
-    component: Aux1Component,
-    outlet: 'aux'
-  },
-  {
-    path: 'aux2',
-    component: Aux2Component,
-    outlet: 'aux'
-  },
-  {
-    path: 'aux3',
-    component: Aux3Component,
-    outlet: 'aux'
-  }];
+    path: 'video',
+    children: [
+      {
+        path: 'aux1',
+        component: Aux1Component,
+        outlet: 'aux'
+      },
+      {
+        path: 'aux2',
+        component: Aux2Component,
+        outlet: 'aux'
+      },
+      {
+        path: 'aux3',
+        component: Aux3Component,
+        outlet: 'aux'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
